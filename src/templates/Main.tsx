3ba417f06c568a076dedd37d72c8fe2bd515c27d
zcvfcat <zcvfcat@gmail.com>
2021-05-24 01:15:23 +0900

    init

---
+++ src/templates/Main.tsx
@@ -0,0 +1,12 @@
+import { Header, Nav, Section, Aside, Footer } from 'layouts/Common'
+export const Main = () => {
+  return (
+    <>
+      <Header></Header>
+      <Nav></Nav>
+      <Section></Section>
+      <Aside></Aside>
+      <Footer></Footer>
+    </>
+  )
+}
